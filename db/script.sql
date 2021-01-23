87c85887198c0be1970717bc74221bd8765ed69b
devansh2001 <dponda3@gatech.edu>
2021-01-23 18:01:02 -0500

    added new add class service

--- db/script.sql
+++ db/script.sql
@@ -13,17 +13,23 @@ CREATE TABLE takes (
 );
 
 CREATE TABLE room (
-    room_id varchar(64) NOT NULL,
+    room_id varchar(128) NOT NULL,
     length integer NOT NULL,
     breadth integer NOT NULL
 );
 
 CREATE TABLE config (
-    room_id varchar(64) NOT NULL,
+    room_id varchar(128) NOT NULL,
     seat_id integer NOT NULL,
     status varchar(16) NOT NULL,
     taken_by varchar(128)
 );
+
+CREATE TABLE class_to_room (
+    room_id varchar(128) NOT NULL,
+    class_id varchar(64),
+    timing DATETIME
+)
 
 
 DROP DATABASE gtemhack;
